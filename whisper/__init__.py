4981322e4753724e25e2ce8ec0437aba88c28a9f
Çağatay Çallı <ccalli@gmail.com>
2022-09-22 16:24:28 +0300

    use MPS if available

--- whisper/__init__.py
+++ whisper/__init__.py
@@ -101,6 +101,6 @@ def load_model(name: str, device: Optional[Union[str, torch.device]] = None, dow
     model.load_state_dict(state_dict)
 
     if device is None:
-        device = "cuda" if torch.cuda.is_available() else "cpu"
+        device = "mps" if torch.backends.mps.is_available() else ("cuda" if torch.cuda.is_available() else "cpu")
 
     return model.to(device)
--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -64,7 +64,9 @@ def transcribe(
     """
     dtype = torch.float16 if decode_options.get("fp16", True) else torch.float32
     if model.device == torch.device("cpu"):
-        if torch.cuda.is_available():
+        if torch.backends.mps.is_available():
+            warnings.warn("Performing inference on CPU when MPS is available")
+        elif torch.cuda.is_available():
             warnings.warn("Performing inference on CPU when CUDA is available")
         if dtype == torch.float16:
             warnings.warn("FP16 is not supported on CPU; using FP32 instead")
@@ -230,7 +232,7 @@ def cli():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("audio", nargs="+", type=str, help="audio file(s) to transcribe")
     parser.add_argument("--model", default="small", choices=available_models(), help="name of the Whisper model to use")
-    parser.add_argument("--device", default="cuda" if torch.cuda.is_available() else "cpu", help="device to use for PyTorch inference")
+    parser.add_argument("--device", default="mps" if torch.backends.mps.is_available() else ("cuda" if torch.cuda.is_available() else "cpu"), help="device to use for PyTorch inference")
     parser.add_argument("--output_dir", "-o", type=str, default=".", help="directory to save the outputs")
     parser.add_argument("--verbose", type=str2bool, default=True, help="Whether to print out the progress and debug messages")
 
